6c64fcb5c18d9d57239766381b9352a111ad08cf
sammern <samreactdev@gmail.com>
2026-07-12 23:28:47 +0300

    feat: install necessary dependencies, configure vite with including svgr to render svgs as react component, css modules with scss injecting mixins and path aliases for clean imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), svgr()],
+  resolve: {
+    alias: {
+      "@/*": path.resolve(__dirname, "./src/*"),
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/shared/styles/mixins" as *',
+      },
+    },
+  },
+});
