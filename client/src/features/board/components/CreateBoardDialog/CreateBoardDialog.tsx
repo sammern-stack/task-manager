95506ebcb17883e75529627d2d2e42b9efcfffc8
sammern <samreactdev@gmail.com>
2026-07-16 08:53:44 +0300

    feat: implement CreateBoardDialog component with form handling and styling

--- client/src/features/board/components/CreateBoardDialog/CreateBoardDialog.tsx
+++ client/src/features/board/components/CreateBoardDialog/CreateBoardDialog.tsx
@@ -1,9 +1,58 @@
 import styles from "./CreateBoardDialog.module.scss";
+import { useState } from "react";
+import { useDialogStore } from "@/shared/stores";
+import { useCreateBoard } from "../../hooks/useBoards";
+import type { FormSubmitEvent } from "@/shared/types/react.types";
 
 export const CreateBoardDialog = () => {
+  const { mutate: createBoard } = useCreateBoard();
+  const [boardName, setBoardName] = useState("");
+  const closeDialog = useDialogStore((s) => s.closeDialog);
+
+  const handleFormSubmit = (e: FormSubmitEvent) => {
+    e.preventDefault();
+    handleNewBoard();
+  };
+
+  const handleNewBoard = () => {
+    createBoard({ name: boardName });
+    closeDialog();
+  };
+
   return (
     <div className={styles.createBoardDialog}>
-      <p>Create board dialog content</p>
+      <h2 className={styles.createBoardDialog__title}>Add New Board</h2>
+      <form
+        className={styles.createBoardDialog__form}
+        onSubmit={handleFormSubmit}
+      >
+        <label
+          htmlFor="boardName"
+          className={styles.createBoardDialog__boardNameLabel}
+        >
+          <span className={styles.createBoardDialog__boardNameLabelText}>
+            Board Name
+          </span>
+          <input
+            type="text"
+            id="boardName"
+            name="boardName"
+            value={boardName}
+            onChange={(e) => setBoardName(e.target.value)}
+            placeholder="e.g. Web Design"
+            autoComplete="off"
+            className={styles.createBoardDialog__input}
+          />
+        </label>
+
+        <button
+          type="submit"
+          className={styles.createBoardDialog__submit}
+          onClick={handleNewBoard}
+        >
+          Create New Board
+        </button>
+      </form>
     </div>
   );
 };
---
+++ client/src/shared/types/react.types.ts
@@ -0,0 +1,3 @@
+import type { SubmitEvent } from "react";
+
+export type FormSubmitEvent = SubmitEvent<HTMLFormElement>;
